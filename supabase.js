b772210606047b18d74cfd2d57d155249d705413
lukevonbergen <57679649+lukevonbergen@users.noreply.github.com>
2024-12-10 20:21:28 +0000

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,1 +1,8 @@
+import { createClient } from '@supabase/supabase-js';
 
+const supabaseUrl = 'https://vhfbwhyfroedggmsdbhs.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZoZmJ3aHlmcm9lZGdnbXNkYmhzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM4Mzc5MTQsImV4cCI6MjA0OTQxMzkxNH0.EmS6PzfNRr3Q93aWY4iNpVrIsmHjhe5v_EnfKyHo4tY';
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export { supabase };
